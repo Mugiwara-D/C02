42461d22842609ac8999cd5c22b19ec7ae2096aa
Matiya DERUELLE <mderuell@e3r1p4.42.fr>
2021-03-22 16:13:57 +0100

    ca vas le faire

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: mderuell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 08:32:53 by mderuell          #+#    #+#             */
-/*   Updated: 2021/03/22 10:19:23 by mderuell         ###   ########.fr       */
+/*   Updated: 2021/03/22 14:57:54 by mderuell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	}
 	while (i < n)
 	{
-		dest[i] = '\n';
+		dest[i] = '\0';
 		i++;
 	}
 	return (dest);
